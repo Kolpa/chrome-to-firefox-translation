c35f07f823c3bc3cc6845cfee7667c66e6d55607
Kolya Opahle <kolyaopahle@gmail.com>
2016-08-06 01:57:39 +0200

    remove test stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 var gulp = require('gulp');
 var ChromeToFirefox = require('./main.js');
-var flatten = require('gulp-flatten');
 
 gulp.task('translate', function() {
 	return gulp.src('_locales/**')
 	.pipe(ChromeToFirefox())
-	.pipe(flatten())
 	.pipe(gulp.dest('localesFF'))
 });
 
